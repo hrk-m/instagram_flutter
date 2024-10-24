aaef70e8034e9065c2605218cb729ee91bd8f81c
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-10-24 20:35:52 +0900

    android, ios, web  app をfirebaseに追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBPfEeG7ivSJFSDSBJWQZTXOuGyFM-VNhc",
+          appId: "1:598779337487:web:371ca86ed8c38a45fb7c7a",
+          messagingSenderId: "598779337487",
+          projectId: "instagram-flutter-fc905",
+          storageBucket: 'instagram-flutter-fc905.appspot.com'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
